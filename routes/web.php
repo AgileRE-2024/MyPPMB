78b4333a49808bb212f56b65a76547dc5a8c9507
darissultan <160088793+javierihsan@users.noreply.github.com>
2024-12-11 15:58:05 +0700

    routing update

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,29 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ExcelMergeController;
-use App\Http\Controllers\ContentController;
-use App\Http\Controllers\NilaiController;
-use App\Http\Controllers\ListContentController;
 use App\Http\Controllers\UserHomeController;
 use App\Http\Controllers\GelombangController;
+use App\Http\Controllers\HostController;
+
 
 Route::post('/excel/merge', [ExcelMergeController::class, 'mergeFiles'])->name('excel.merge');
+Route::post('/excel/import/{id_gelombang}', [ExcelMergeController::class, 'importFiles'])->name('excel.import');
+
 Route::get('/', [LoginController::class, 'index']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+
 Route::get('/home', [HomeController::class, 'index']);
-Route::get('/konten', [ContentController::class, 'index']);
-Route::get('/nilai', [NilaiController::class, 'index']);
-Route::get('/listkonten', [ListContentController::class, 'index']);
+Route::get('/nilai', [HomeController::class, 'nilai']);
+Route::get('/pimpinan', [HomeController::class, 'pimpinan']);
+Route::get('/download/{id_gelombang}', [HomeController::class, 'download'])->name('download');
+
+
 Route::get('/userhome', [UserHomeController::class, 'index']);
+Route::post('/update-detail/{id}', [UserHomeController::class, 'updateDetail'])->name('updateDetail');
+
 Route::post('/gelombang/store', [GelombangController::class, 'store'])->name('gelombang.store');
-Route::post('/gelombang/{id_gelombang}', [GelombangController::class, 'destroy'])->name('delete.store');
+Route::post('/gelombang/{id_gelombang}', action: [GelombangController::class, 'destroy'])->name('delete.store');
+Route::post('/gelombang/{id}/toggle-status', [GelombangController::class, 'toggleStatus'])->name('gelombang.toggleStatus');
+
+Route::get('/host', [HostController::class, 'index']);
